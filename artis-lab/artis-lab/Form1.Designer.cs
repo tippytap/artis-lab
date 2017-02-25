71371a130c08ae447f42111c1cccba1da93bafb6
Erik Miller <emiller46@radford.edu>
2017-02-25 18:05:59 -0500

    Added top menu and menu buttons

--- artis-lab/artis-lab/Form1.Designer.cs
+++ artis-lab/artis-lab/Form1.Designer.cs
@@ -28,20 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuUsers = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuMyAccount = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuBtnManageUsers = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuBtnAddNewUser = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuBtnSeeTopUsers = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuBtnLogIn = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuBtnLogOut = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuUsers,
+            this.menuMyAccount});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuUsers
+            // 
+            this.menuUsers.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuBtnManageUsers,
+            this.menuBtnAddNewUser,
+            this.menuBtnSeeTopUsers});
+            this.menuUsers.Name = "menuUsers";
+            this.menuUsers.Size = new System.Drawing.Size(47, 20);
+            this.menuUsers.Text = "Users";
+            // 
+            // menuMyAccount
+            // 
+            this.menuMyAccount.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuBtnLogIn,
+            this.menuBtnLogOut});
+            this.menuMyAccount.Name = "menuMyAccount";
+            this.menuMyAccount.Size = new System.Drawing.Size(84, 20);
+            this.menuMyAccount.Text = "My Account";
+            // 
+            // menuBtnManageUsers
+            // 
+            this.menuBtnManageUsers.Name = "menuBtnManageUsers";
+            this.menuBtnManageUsers.Size = new System.Drawing.Size(158, 22);
+            this.menuBtnManageUsers.Text = "Manage Users...";
+            // 
+            // menuBtnAddNewUser
+            // 
+            this.menuBtnAddNewUser.Name = "menuBtnAddNewUser";
+            this.menuBtnAddNewUser.Size = new System.Drawing.Size(158, 22);
+            this.menuBtnAddNewUser.Text = "Add New User...";
+            // 
+            // menuBtnSeeTopUsers
+            // 
+            this.menuBtnSeeTopUsers.Name = "menuBtnSeeTopUsers";
+            this.menuBtnSeeTopUsers.Size = new System.Drawing.Size(158, 22);
+            this.menuBtnSeeTopUsers.Text = "See Top Users";
+            // 
+            // menuBtnLogIn
+            // 
+            this.menuBtnLogIn.Name = "menuBtnLogIn";
+            this.menuBtnLogIn.Size = new System.Drawing.Size(152, 22);
+            this.menuBtnLogIn.Text = "Log In..";
+            // 
+            // menuBtnLogOut
+            // 
+            this.menuBtnLogOut.Name = "menuBtnLogOut";
+            this.menuBtnLogOut.Size = new System.Drawing.Size(152, 22);
+            this.menuBtnLogOut.Text = "Log Out";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.Text = "ARTIS Lab Admin";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuUsers;
+        private System.Windows.Forms.ToolStripMenuItem menuBtnManageUsers;
+        private System.Windows.Forms.ToolStripMenuItem menuBtnAddNewUser;
+        private System.Windows.Forms.ToolStripMenuItem menuBtnSeeTopUsers;
+        private System.Windows.Forms.ToolStripMenuItem menuMyAccount;
+        private System.Windows.Forms.ToolStripMenuItem menuBtnLogIn;
+        private System.Windows.Forms.ToolStripMenuItem menuBtnLogOut;
     }
 }
 
